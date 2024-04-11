76539490895a15b7c7fbf2b5ae6a9758d982873c
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-04-11 14:37:15 +1000

    add `error`, `loading`, and `score` states, create `fetchNewQuestions()` to get questions on call, display score after game completion, allow user to play new game

--- frontend/src/pages/Play/Play.tsx
+++ frontend/src/pages/Play/Play.tsx
@@ -1,41 +1,85 @@
 import { useState, useEffect } from 'react';
-import MainContainer from '../../containers/MainContainer/MainContainer';
+import MainContainer from '../../containers/MainContainer/MainContainer.tsx';
 import { Question } from '../../lib/definitions';
 import { getQuestions } from '../../services/opentdb';
 import Trivia from '../../components/Trivia/Trivia';
 
 const Play = () => {
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
   const [questions, setQuestions] = useState<Question[] | null>(null);
   const [currentQuestion, setCurrentQuestion] = useState<number | null>(null);
-  const [score, setScore] = useState<number>(0);
+  const [score, setScore] = useState<number | null>(null);
   const [gameOver, setGameOver] = useState<boolean>(true);
 
-  useEffect(() => {
+  const fetchNewQuestions = () => {
+    setLoading(true);
     getQuestions()
-      .then((res) => setQuestions(res))
-      .catch((err) => console.warn(err.message));
+      .then((res) => {
+        setQuestions(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchNewQuestions();
   }, []);
 
-  console.log(questions);
+  useEffect(() => {
+    if (questions && currentQuestion !== null) {
+      if (currentQuestion > questions.length - 1) {
+        setGameOver(true);
+        setQuestions(null);
+        setCurrentQuestion(null);
+      }
+    }
+  }, [currentQuestion]);
 
   return (
     <MainContainer>
-      {questions && gameOver && !currentQuestion && !score && (
+      {loading && <p>Loading...</p>}
+      {error && !loading && (
         <>
-          <p>Ready?</p>
+          <p>{error}</p>
           <button
             onClick={() => {
-              setGameOver(false);
-              setCurrentQuestion(0);
+              setError(null);
+              fetchNewQuestions();
             }}
           >
-            Start
+            Try again
           </button>
         </>
       )}
-      {questions &&
+      {!loading &&
+        !error &&
+        questions &&
+        gameOver &&
+        !currentQuestion &&
+        !score && (
+          <>
+            <p>Ready?</p>
+            <button
+              onClick={() => {
+                setGameOver(false);
+                setScore(0);
+                setCurrentQuestion(0);
+              }}
+            >
+              Start
+            </button>
+          </>
+        )}
+      {!loading &&
+        !error &&
+        questions &&
         currentQuestion !== null &&
         currentQuestion < questions.length &&
+        score !== null &&
         !gameOver && (
           <>
             <p>{score}</p>
@@ -48,6 +92,20 @@ const Play = () => {
             />
           </>
         )}
+      {!loading && !error && gameOver && score !== null && (
+        <>
+          <p>Game over!</p>
+          <p>{score}</p>
+          <button
+            onClick={() => {
+              fetchNewQuestions();
+              setScore(null);
+            }}
+          >
+            Play again
+          </button>
+        </>
+      )}
     </MainContainer>
   );
 };
