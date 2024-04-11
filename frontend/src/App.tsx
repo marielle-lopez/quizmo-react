4b7fc53d7570c34f42d903bf1887fb75157257b3
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-04-11 13:39:28 +1000

    remove default code from application setup and add routing to different pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home/Home';
+import Play from './pages/Play/Play';
+import Profile from './pages/Profile/Profile';
+import Header from './containers/Header/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Play />} path="/play" />
+          <Route element={<Profile />} path="/profile" />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
