ce3e10db04572be72b3034f174d97779db1f91df
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-04-12 13:34:50 +1000

    add `getCategories()` to fetch categories

--- frontend/src/services/opentdb.ts
+++ frontend/src/services/opentdb.ts
@@ -1,6 +1,6 @@
 export const getQuestions = async () => {
   const response = await fetch(
-    `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`
+    `https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple`
   );
 
   if (!response.ok) {
@@ -9,4 +9,15 @@ export const getQuestions = async () => {
 
   const data = await response.json();
   return data.results;
+};
+
+export const getCategories = async () => {
+  const response = await fetch(`https://opentdb.com/api_category.php`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch categories');
+  }
+
+  const data = await response.json();
+  return data.trivia_categories;
 };
