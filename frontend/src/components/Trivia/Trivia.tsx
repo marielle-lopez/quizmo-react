4d2f33132b3a87d747e1939801ae2cc229c7c86a
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-04-15 18:49:34 +1000

    create `progress` state and update `progress` every second, pass `progress` state as prop to `Countdown` component

--- frontend/src/components/Trivia/Trivia.tsx
+++ frontend/src/components/Trivia/Trivia.tsx
@@ -1,37 +1,54 @@
+import { useEffect, useState } from 'react';
 import { Question } from '../../lib/definitions';
 import { decodeHtmlEntities } from '../../lib/functions/decode-html-entities';
-import { shuffleArray } from '../../lib/functions/shuffle-array';
-// import Countdown from '../Countdown/Countdown';
+import Countdown from '../Countdown/Countdown';
 
 const Trivia = ({
   question,
   currentQuestion,
   setCurrentQuestion,
+  currentAnswers,
   score,
   setScore,
 }: {
   question: Question;
   currentQuestion: number;
   setCurrentQuestion: (currentQuestion: number) => void;
+  currentAnswers: string[];
   score: number;
   setScore: (score: number) => void;
 }) => {
-  const shuffledAnswers = shuffleArray([
-    question.correct_answer,
-    ...question.incorrect_answers,
-  ]);
+  const [progress, setProgress] = useState<number>(0);
 
-  const timer = setTimeout(() => {
-    setCurrentQuestion(currentQuestion + 1);
-  }, 5000);
+  let timer: any;
+
+  useEffect(() => {
+    timer = setInterval(() => {
+      if (progress < 100) {
+        console.log(progress);
+        setProgress(progress + 20);
+        return;
+      }
+      if (progress === 100) {
+        setProgress(0);
+        setCurrentQuestion(currentQuestion + 1);
+        clearInterval(timer);
+        return;
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  });
+
   return (
     <div>
       <p>{decodeHtmlEntities(question.question)}</p>
-      {shuffledAnswers.map((answer) => (
+      {currentAnswers.map((answer) => (
         <button
           key={answer}
           onClick={() => {
-            clearTimeout(timer);
+            clearInterval(timer);
+            setProgress(0);
             decodeHtmlEntities(answer) ===
               decodeHtmlEntities(question.correct_answer) &&
               setScore(score + 1);
@@ -41,12 +58,9 @@ const Trivia = ({
           {decodeHtmlEntities(answer)}
         </button>
       ))}
-      {/* <div>
-        <Countdown
-          currentQuestion={currentQuestion}
-          setCurrentQuestion={setCurrentQuestion}
-        />
-      </div> */}
+      <div>
+        <Countdown progress={progress} />
+      </div>
     </div>
   );
 };
