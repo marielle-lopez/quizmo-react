979a3bfc4d867c3e7c776e5435c3c9223f86d433
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-04-15 18:47:05 +1000

    remove `useEffect` using `setInterval()`, accept `progress` as a prop

--- frontend/src/components/Countdown/Countdown.tsx
+++ frontend/src/components/Countdown/Countdown.tsx
@@ -1,32 +1,4 @@
-import { useEffect, useState } from 'react';
-
-const Countdown = ({
-  currentQuestion,
-  setCurrentQuestion,
-}: {
-  currentQuestion: number;
-  setCurrentQuestion: (currentQuestion: number) => void;
-}) => {
-  const [progress, setProgress] = useState<number>(0);
-
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      if (progress < 100) {
-        console.log(progress);
-        setProgress(progress + 20);
-        return;
-      }
-      if (progress === 100) {
-        setProgress(0);
-        setCurrentQuestion(currentQuestion + 1);
-        clearInterval(timerId);
-        return;
-      }
-    }, 1000);
-
-    return () => clearInterval(timerId);
-  });
-
+const Countdown = ({ progress }: { progress: number }) => {
   return (
     <>
       <progress value={progress} max={100}>
