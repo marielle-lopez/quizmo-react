73a227bf5af438f0fd6b7b7cab56a5af10a5d0fc
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-04-15 17:58:27 +1000

    remove unnecessary React fragments

--- frontend/src/components/Trivia/Trivia.tsx
+++ frontend/src/components/Trivia/Trivia.tsx
@@ -1,6 +1,7 @@
 import { Question } from '../../lib/definitions';
 import { decodeHtmlEntities } from '../../lib/functions/decode-html-entities';
 import { shuffleArray } from '../../lib/functions/shuffle-array';
+// import Countdown from '../Countdown/Countdown';
 
 const Trivia = ({
   question,
@@ -19,29 +20,33 @@ const Trivia = ({
     question.correct_answer,
     ...question.incorrect_answers,
   ]);
+
   const timer = setTimeout(() => {
     setCurrentQuestion(currentQuestion + 1);
   }, 5000);
-
   return (
     <div>
       <p>{decodeHtmlEntities(question.question)}</p>
       {shuffledAnswers.map((answer) => (
-        <>
-          <button
-            key={answer}
-            onClick={() => {
-              clearTimeout(timer);
-              decodeHtmlEntities(answer) ===
-                decodeHtmlEntities(question.correct_answer) &&
-                setScore(score + 1);
-              setCurrentQuestion(currentQuestion + 1);
-            }}
-          >
-            {decodeHtmlEntities(answer)}
-          </button>
-        </>
+        <button
+          key={answer}
+          onClick={() => {
+            clearTimeout(timer);
+            decodeHtmlEntities(answer) ===
+              decodeHtmlEntities(question.correct_answer) &&
+              setScore(score + 1);
+            setCurrentQuestion(currentQuestion + 1);
+          }}
+        >
+          {decodeHtmlEntities(answer)}
+        </button>
       ))}
+      {/* <div>
+        <Countdown
+          currentQuestion={currentQuestion}
+          setCurrentQuestion={setCurrentQuestion}
+        />
+      </div> */}
     </div>
   );
 };
