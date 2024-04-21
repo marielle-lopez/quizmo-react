8b6b637e29bcc6e41fd4582bcdb57e85fb9d6ed0
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-04-21 21:31:38 +1000

    connect repository and `ModelMapper`, implement methods to handle available HTTP request methods supported in controller layer

--- backend/src/main/java/marielle/lopez/quizmo/games/GameService.java
+++ backend/src/main/java/marielle/lopez/quizmo/games/GameService.java
@@ -1,25 +1,54 @@
 package marielle.lopez.quizmo.games;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @Service
 @Transactional
 public class GameService {
-	public String getAllGames() {
-		return "This gets all games";
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<Game> getAllGames() {
+		return this.gameRepository.findAll();
 	};
 	
-	public String saveGame() {
-		return "This saves a game";
+	public Game createGame(@Valid CreateGameDTO data) {
+		Game newGame = modelMapper.map(data, Game.class);
+		return this.gameRepository.save(newGame);
 	};
 	
-	public String updateGame() {
-		return "This updates a game";
+	public Optional<Game> updateGameById(Long id, @Valid UpdateGameDTO data) {
+		Optional<Game> maybeGame = this.gameRepository.findById(id);
+		if (maybeGame.isEmpty()) {
+			return maybeGame;
+		};
+		Game foundGame = maybeGame.get();
+		modelMapper.map(data, foundGame);
+		Game updatedGame = this.gameRepository.save(foundGame);
+		return Optional.of(updatedGame);
 	};
 	
-	public String deleteGame() {
-		return "This deletes a game";
+	public boolean deleteGameById(Long id) {
+		Optional<Game> maybeGame = this.gameRepository.findById(id);
+		if (maybeGame.isEmpty()) {
+			return false;
+		};
+		this.gameRepository.delete(maybeGame.get());
+		return true;
+	}
+
+	public Optional<Game> getGameById(Long id) {
+		return this.gameRepository.findById(id);
 	};
 }
